334a5ee999950b75d1c847c8287e092f29bfe7d0
Antto <antto.hautamaki@elisanet.fi>
2023-05-22 10:35:10 +0300

    first version of status.ok tests.....

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,19 +1,33 @@
 
 using System.IO;
 
-namespace TestProject1
+namespace Test
 {
     [TestClass]
-    public class UnitTest1
+    public class CycleApitests
     {
 
         [TestMethod]
         public   void TestJourneyAPi()
         {
             HttpClient client = new HttpClient();
-           
             HttpResponseMessage response =  client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/Journeys?page=1").Result;
-Console.WriteLine(response.StatusCode); 
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void TestStationsAPi()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/Stations?page=1").Result;
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void TestStationsdetailsAPi()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/stationdetails?id=001").Result;
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
         }
 
