d58db420ba66c5bd86ebc71ff8b8d3f89281a6a4
Antto <antto.hautamaki@elisanet.fi>
2023-05-22 12:08:46 +0300

    Version 1.0 of tests ready

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,8 +1,20 @@
 
 using System.IO;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Newtonsoft.Json;
 
 namespace Test
 {
+    struct Station
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public int NumDeb { get; set; }
+        public int NumEnd { get; set; }
+    };
     [TestClass]
     public class CycleApitests
     {
@@ -10,7 +22,7 @@ namespace Test
         [TestMethod]
         public   void TestJourneyAPi()
         {
-            HttpClient client = new HttpClient();
+            HttpClient client = new ();
             HttpResponseMessage response =  client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/Journeys?page=1").Result;
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
         }
@@ -18,17 +30,29 @@ namespace Test
         [TestMethod]
         public void TestStationsAPi()
         {
-            HttpClient client = new HttpClient();
+            HttpClient client = new();
             HttpResponseMessage response = client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/Stations?page=1").Result;
+
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
         }
 
         [TestMethod]
         public void TestStationsdetailsAPi()
         {
-            HttpClient client = new HttpClient();
+            HttpClient client = new ();
             HttpResponseMessage response = client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/stationdetails?id=001").Result;
+           
+
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void TestStationsdetailsContentAPi()
+        {
+            HttpClient client = new();
+            HttpResponseMessage response = client.GetAsync("https://functionapp220230415180354.azurewebsites.net/api/stationdetails?id=001").Result;
+            Station deserializedProduct = JsonConvert.DeserializeObject<Station>(response.Content.ReadAsStringAsync().Result);
+            Assert.AreEqual(deserializedProduct.Name, "Kaivopuisto");
         }
 
     }
